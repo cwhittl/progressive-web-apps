b4a288076e6b4311dca51aa51718e93330ffdcf2
Chris Whittle <cwhittl@gmail.com>
2016-12-31 14:55:35 -0600

    using hack to get around having 2 index.htmls

--- car-deals/js/swRegister.js
+++ car-deals/js/swRegister.js
@@ -1,46 +1,49 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-  .register('sw.js', {scope:''})
-  .then(function(swRegistration) { 
-    
+  navigator.serviceWorker.register('sw.js', {scope: ''}).then(function(swRegistration) {
+
     var serviceWorker;
 
-    if(swRegistration.installing){
+    if (swRegistration.installing) {
       console.log('Resolved at installing:  ', swRegistration);
       serviceWorker = swRegistration.installing;
-    } else if (swRegistration.waiting){
+    } else if (swRegistration.waiting) {
       console.log('Resolved at installed/waiting: ', swRegistration);
-      serviceWorker = swRegistration.waiting; 
-    } else if (swRegistration.active){
+      serviceWorker = swRegistration.waiting;
+    } else if (swRegistration.active) {
       console.log('Resolved at activated: ', swRegistration);
-      serviceWorker = swRegistration.active; 
+      serviceWorker = swRegistration.active;
     }
 
     if (serviceWorker) {
-      serviceWorker.addEventListener('statechange', function (e) {
-          console.log(e.target.state);
+      serviceWorker.addEventListener('statechange', function(e) {
+        console.log(e.target.state);
       });
     }
 
-    swRegistration.addEventListener('updatefound', function(e){
-      swRegistration.installing.addEventListener('statechange', function (e) {
-          console.log("New service worker state: ", e.target.state);
+    swRegistration.addEventListener('updatefound', function(e) {
+      swRegistration.installing.addEventListener('statechange', function(e) {
+        console.log("New service worker state: ", e.target.state);
       });
       console.log("New service worker found!", swRegistration)
     });
 
     //Check for an update every hour
-    setInterval(function(){
+    setInterval(function() {
       swRegistration.update();
-    },1000*60*60);
+    }, 1000 * 60 * 60);
 
   }).catch(function(error) {
     console.log('Error occurred', error);
   });
-  
+
   //An extra event that is fired when the service worker controlling this page changes
   //through the self.skipWaiting()
-  navigator.serviceWorker.addEventListener('controllerchange', function(e){
+  navigator.serviceWorker.addEventListener('controllerchange', function(e) {
     console.log("Controller Changed!");
   });
-} 
+} else if ('applicationCache' in window) {
+  var iframe = document.createElement('iframe');
+  iframe.style.display = 'none';
+  iframe.src = 'load-appcache.html'
+  document.body.appendChild(iframe);
+}
